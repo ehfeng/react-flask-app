2c0f818af2e337e373b33cfdd74e12cfb069c6f5
Eric Feng <erichfeng@gmail.com>
2018-11-25 18:04:47 -0600

    updating

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__, static_url_path='', static_folder='static/dist')
+app = Flask(__name__)
 
 
 @app.route("/")
--- static/src/index.js
+++ static/src/index.js
@@ -1,22 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './helloMessage';
 import './index.scss';
-
-
-class HelloMessage extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello {this.props.name}
-      </div>
-    );
-  }
-}
 
 var mountNode = document.getElementById('root')
 
 ReactDOM.render(
-  <HelloMessage name="Eric" />,
+  <HelloMessage name="React" />,
   mountNode
 );
